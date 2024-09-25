616ca958425975b5c99a7fe867b9553f8f9771c0
emymariuzzi <161320976+emymariuzzi@users.noreply.github.com>
2024-09-25 11:34:01 -0300

    ç

--- main.js
+++ main.js
@@ -4,6 +4,6 @@
             return numero1+numero2;
            }
            console.log (soma (2,3));
-           function soma (numero1*numero2){
-            return numero1*numero2;
+           function calcularAreaQuadrado(lado){
+            return lado*lado;
            }
