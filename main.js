560a3f5ef16e34adb28776921bae7776cb9203be
emymariuzzi <161320976+emymariuzzi@users.noreply.github.com>
2024-09-25 11:59:19 -0300

    k-

--- main.js
+++ main.js
@@ -8,7 +8,7 @@
             return lado*lado;
            }
            console.log (calcularAreaQuadrado (8));
-           let media= function (num4num5num6){
+           let media= function (numero4numero5numero6){
             return x+y+z/3 ;
            }
            console.log ()
