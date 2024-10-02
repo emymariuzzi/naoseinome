827f8ae6641220808325aab6c217e94cf81b9d18
emymariuzzi <161320976+emymariuzzi@users.noreply.github.com>
2024-10-02 11:25:32 -0300

    K

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 
-           alert ('emilly mariuzzi e maria luiza')
-           function soma (numero1, numero2){
-            return numero1+numero2;
-           }
-           console.log (soma (2,3));
-           function calcularAreaQuadrado(lado){
-            return lado*lado;
-           }
-           console.log (calcularAreaQuadrado (8));
-           let media= function (x,y,z){
-            return x+y+z/3 ;
-           }
-           console.log (media (10,10,10))
-           
+        //    alert ('emilly mariuzzi e maria luiza')
+        //    function soma (numero1, numero2){
+        //     return numero1+numero2;
+        //    }
+        //    console.log (soma (2,3));
+        //    function calcularAreaQuadrado(lado){
+        //     return lado*lado;
+        //    }
+        //    console.log (calcularAreaQuadrado (8));
+        //    let media= function (x,y,z){
+        //     return x+y+z/3 ;
+        //    }
+        //    console.log (media (10,10,10))
+
+    let estados = ['PR','RJ',...]
+        
