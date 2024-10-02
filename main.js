d28400000eb29fa27246e547d2095caa40b2da18
emymariuzzi <161320976+emymariuzzi@users.noreply.github.com>
2024-10-02 12:13:36 -0300

    l

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 
-        //    alert ('emilly mariuzzi e maria luiza')
-        //    function soma (numero1, numero2){
-        //     return numero1+numero2;
-        //    }
-        //    console.log (soma (2,3));
-        //    function calcularAreaQuadrado(lado){
-        //     return lado*lado;
-        //    }
-        //    console.log (calcularAreaQuadrado (8));
-        //    let media= function (x,y,z){
-        //     return x+y+z/3 ;
-        //    }
-        //    console.log (media (10,10,10))
+//    alert ('emilly mariuzzi e maria luiza')
+//    function soma (numero1, numero2){
+//     return numero1+numero2;
+//    }
+//    console.log (soma (2,3));
+//    function calcularAreaQuadrado(lado){
+//     return lado*lado;
+//    }
+//    console.log (calcularAreaQuadrado (8));
+//    let media= function (x,y,z){
+//     return x+y+z/3 ;
+//    }
+//    console.log (media (10,10,10))
 
-    let estados = ['PR','RJ','MT','MG','AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MS','PA','PB','PE','PI','RN','RO','RS','SC','SP','SE','TO']
-    let i=0;
-    while(i<estados.length){
-console.log(i);
-console.log(estados[i]);
-i=i+1;
-     } 
-    for(let i=0;estados.length;i++ ){
-        console.log(i);
-        console.log(estados[i]);
-    }   
+let estados = ['PR', 'RJ', 'MT', 'MG', 'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MS', 'PA', 'PB', 'PE', 'PI', 'RN', 'RO', 'RS', 'SC', 'SP', 'SE', 'TO']
+let i = 0;
+while (i < estados.length) {
+    console.log(i);
+    console.log(estados[i]);
+    i = i + 1;
+}
+for (let i = 0; estados.length; i++) {
+    console.log(i);
+    console.log(estados[i]);
+}   
